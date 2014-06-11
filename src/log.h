c0aaae1987f4165b19cb1772027598ae9e5c692d
Courtney Cavin <courtney.cavin@sonymobile.com>
2014-06-11 14:29:48 -0700

    log: add V (verbose) and W (warning) logging levels

--- src/log.h
+++ src/log.h
@@ -4,13 +4,17 @@
 #ifdef ANDROID
 
 #include <cutils/log.h>
+#define LOGV ALOGV
 #define LOGI ALOGI
+#define LOGW ALOGW
 #define LOGE ALOGE
 
 #elif defined(__linux__)
 
 #include <stdio.h>
+#define LOGV(x, ...) fprintf(stderr, "[V] "LOG_TAG": " x, ##__VA_ARGS__)
 #define LOGI(x, ...) fprintf(stderr, "[I] "LOG_TAG": " x, ##__VA_ARGS__)
+#define LOGW(x, ...) fprintf(stderr, "[W] "LOG_TAG": " x, ##__VA_ARGS__)
 #define LOGE(x, ...) fprintf(stderr, "[E] "LOG_TAG": " x, ##__VA_ARGS__)
 
 #endif
